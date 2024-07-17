6a6e9f6869f1ec5b5c59209c421733834f9703bc
Wang kongwu <1435212918@qq.com>
2024-07-17 09:43:15 +0800

    字符串逆序

---
+++ CounterStr/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CounterStr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            char[] chars;
+            while (true)
+            {
+                Console.Write("请输入一串字符：");
+                str = Console.ReadLine();
+
+                if (str == "")
+                {
+                    return;
+                }
+
+                chars = str.ToCharArray();
+                Array.Reverse(chars);
+                string s = new string(chars);
+
+                Console.WriteLine($"逆序后的字符串：{s}");
+            }
+        }
+    }
+}
