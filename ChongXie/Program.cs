d2b34fc0bb03eed9440209c01477fe276b425202
Wang kongwu <1435212918@qq.com>
2024-07-19 16:08:40 +0800

    重写

---
+++ ChongXie/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ChongXie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Action();
+
+            Person teacher = new Teacher();
+            teacher.Action();
+        }
+    }
+
+    class Person
+    {
+        public virtual void Action() {
+            Console.WriteLine("This is Person");
+        }
+    }
+
+    class Teacher : Person
+    {
+        public override void Action()
+        {
+            Console.WriteLine("This is Teacher");
+        }
+    }
+}
