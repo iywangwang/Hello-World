ad01caf1d1a06c59989c3b4c0cac537a5ccff561
Wang kongwu <1435212918@qq.com>
2024-07-19 10:11:37 +0800

    事件完整声明

---
+++ CustomerAndWaiter/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.ComponentModel;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CustomerAndWaiter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            Waiter  waiter = new Waiter();
+            customer.Order += waiter.Action;
+
+            customer.Action();
+            customer.PayTheBill();
+        }
+    }
+
+    public class OrderEventArgs : EventArgs
+    {
+        public string DishName { get; set; }
+        public string DishSize { get; set; }
+    }
+
+    public delegate void OrderEventHandler(Customer customer, OrderEventArgs e);
+
+    public class Customer   //顾客
+    {
+        private OrderEventHandler orderEventHandler;
+
+        public event OrderEventHandler Order        //声明事件
+        { 
+            add 
+            { 
+                this.orderEventHandler += value; 
+            }
+
+            remove 
+            {
+                this.orderEventHandler -= value;
+            } 
+        }
+        public double Bill { get; set; }        //账单
+
+        public void PayTheBill()        //结账
+        {
+            Console.WriteLine("Bill is ${0}", this.Bill);
+        }
+
+        public void WalkIn()
+        { 
+            Console.WriteLine("Waik to restaurant");
+        }
+
+        public void SitDown()
+        { 
+            Console.WriteLine("Sit Down");
+        }
+
+        public void Think()
+        {
+            for (int i = 0; i < 5; i++)
+            { 
+                Console.WriteLine("Thinking {0}s.....", i);
+                Thread.Sleep(1000);
+            }
+
+            if (this.orderEventHandler != null)
+            {
+                OrderEventArgs e = new OrderEventArgs() { DishName = "剁椒鱼头", DishSize = "small"};
+
+                this.orderEventHandler.Invoke(this, e);
+            }
+        }
+
+        public void Action()
+        { 
+            this.WalkIn();
+            this.SitDown();
+            this.Think();
+        }
+    }
+
+    public class Waiter
+    {
+        public void Action(Customer customer, OrderEventArgs e)     //处理事件
+        {
+            Console.WriteLine("I will server you the dish {0}", e.DishName);
+            double price = 77;
+
+            switch (e.DishSize)
+            {
+                case "small":
+                    price *= 0.75;
+                    break;
+                case "large":
+                    price *= 1.25;
+                    break;
+                default:
+                    break;
+            }
+
+            customer.Bill += price;
+        }
+    }
+}
