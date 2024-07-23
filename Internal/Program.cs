a294e4af19195e822e8817031649016664c8cbb4
Wang kongwu <1435212918@qq.com>
2024-07-23 08:30:01 +0800

    常用控件和数据模版

---
+++ Internal/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Internal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Engine
+    { 
+        public int RPM { get; private set; }
+    }
+}
---
+++ ListBox_WPF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListBox_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Color> l = new List<Color>();
+
+            l.Add(new Color() { color = "#DB7093", Colorname = "苍白的紫罗兰红色" });
+            l.Add(new Color() { color = "#FF69B4", Colorname = "热情的粉红" });
+            l.Add(new Color() { color = "#FF1493", Colorname = "深粉色" });
+            l.Add(new Color() { color = "#C71585", Colorname = "适中的紫罗兰红色" });
+            l.Add(new Color() { color = "#DA70D6", Colorname = "兰花的紫色" });
+
+            myGrid.ItemsSource = l;
+        }
+
+        class Color
+        {
+            public string color { get; set; }
+            public string Colorname { get; set; }
+        }
+    }
+}
