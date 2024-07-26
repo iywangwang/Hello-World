34f9229b276d13dec1b3fee6ec1b26f2a20a30d7
Wang kongwu <1435212918@qq.com>
2024-07-26 15:36:32 +0800

    附加事件

---
+++ WPFSubjoinEvent/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFSubjoinEvent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //为最外层的Gird实现侦听
+            Staff.AddSalaryChangedHandler(this.BaseGrid, new RoutedEventHandler(this.StaffSalaryChangedHandler));
+        }
+
+        //使用按钮触发事件，并进行路由传递
+        public void MainButton_Click(object sender, RoutedEventArgs e)
+        {
+            Staff staff = new Staff() { Name = "XiaoZhang", Description = 6666.0};
+            staff.Description = 8888.0;
+
+            //借助Button将附加事件进行路由传递
+            RoutedEventArgs args = new RoutedEventArgs(Staff.SalaryChangeEvent, staff);
+            this.BaseGrid.RaiseEvent(args);
+        }
+
+        //监听者监听到事件后的事件处理程序
+        private void StaffSalaryChangedHandler(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show((e.OriginalSource as Staff).Name.ToString() + "的工资变化了");
+        }
+    }
+}
---
+++ WPFSubjoinEvent/Staff.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFSubjoinEvent
+{
+    public class Staff      //职工
+    {
+        public string Name { get; set; }
+        public double Description { get; set; }     //薪水
+
+        public static readonly RoutedEvent SalaryChangeEvent = EventManager.RegisterRoutedEvent
+            ("SalaryChanged", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(Staff));
+
+        public static void AddSalaryChangedHandler(DependencyObject d, RoutedEventHandler h)
+        {
+            UIElement uIE = d as UIElement;
+            if (uIE != null)
+            {
+                uIE.AddHandler(Staff.SalaryChangeEvent, h);
+            }
+        }
+
+        public static void RemoveSalaryChangedHandler(DependencyObject d, RoutedEventHandler h)
+        {
+            UIElement uIE = d as UIElement;
+            if (uIE != null)
+            {
+                uIE.RemoveHandler(Staff.SalaryChangeEvent, h);
+            }
+        }
+    }
+}
